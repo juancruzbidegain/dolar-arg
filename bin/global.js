b5d6c5f529a3664c97274e8c02da478269ab7ee1
juancbidegain <juancruzbidegain@gmail.com>
2022-05-29 19:03:22 -0300

    First commit

---
+++ bin/global.js
@@ -0,0 +1,1 @@
+const cotizacion = require("../src/index")
---
+++ src/request.js
@@ -0,0 +1,8 @@
+const TYPES_REQUEST_USD = {
+    DOLAR_BLUE_USD: "DB",
+    DOLAR_OFICIAL_USD: "DO",
+    BITCOIN_USD: "BTC"
+}
+
+module.exports = TYPES_REQUEST_USD;
+
